200d6e7dfd0dda3e92927e21195ec71e6223d812
jgarcia77 <jgarcia773@gmail.com>
2017-11-23 10:51:01 -0600

    Using Plugins

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
 console.log('App Loaded!');
 
-var config = require('../config/config.json');
-
-console.log(config.app_loaded_msg);
+$('#testDiv').text('jQuery modified this content (see app.js)');
